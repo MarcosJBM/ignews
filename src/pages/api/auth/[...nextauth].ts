05f82228f7be3ff23b37913f37ff9f969e54a562
Marcos Botene <marcosjbotene@gmail.com>
2023-03-26 15:10:50 -0300

    feat: authentication with GitHub using NextAuth.js

---
+++ src/pages/api/auth/[...nextauth].ts
@@ -0,0 +1,18 @@
+import NextAuth, { AuthOptions } from 'next-auth';
+import GithubProvider from 'next-auth/providers/github';
+
+export const authOptions: AuthOptions = {
+  providers: [
+    GithubProvider({
+      clientId: process.env.GITHUB_ID as string,
+      clientSecret: process.env.GITHUB_SECRET as string,
+      authorization: {
+        params: {
+          scope: 'read:user',
+        },
+      },
+    }),
+  ],
+};
+
+export default NextAuth(authOptions);
