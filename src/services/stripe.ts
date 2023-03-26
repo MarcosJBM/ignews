75882c0c556c29f63173e674bdcf789a76b9ee27
Marcos Botene <marcosjbotene@gmail.com>
2023-03-26 15:08:51 -0300

    chore: next-auth installed

--- src/services/stripe.ts
+++ src/services/stripe.ts
@@ -1,11 +1,11 @@
 import Stripe from 'stripe';
 
-import { version } from '../../package.json';
+import packageJson from '../../package.json';
 
 export const stripe = new Stripe(process.env.STRIPE_API_KEY as string, {
   apiVersion: '2022-11-15',
   appInfo: {
     name: 'ig.news',
-    version,
+    version: packageJson.version,
   },
 });
