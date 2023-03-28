f14962922480c06831777e63b499a82f5e4daad1
Marcos Botene <marcosjbotene@gmail.com>
2023-03-27 23:22:12 -0300

    feat: saving user email in fauna database

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,5 +1,7 @@
+import { fauna } from '@/services';
 import NextAuth, { AuthOptions } from 'next-auth';
 import GithubProvider from 'next-auth/providers/github';
+import { query as q } from 'faunadb';
 
 export const authOptions: AuthOptions = {
   providers: [
@@ -9,6 +11,19 @@ export const authOptions: AuthOptions = {
     }),
   ],
   secret: process.env.NEXTAUTH_SECRET,
+  callbacks: {
+    async signIn(user) {
+      try {
+        await fauna.query(
+          q.Create(q.Collection('users'), { data: { email: user.user.email } })
+        );
+
+        return true;
+      } catch {
+        return false;
+      }
+    },
+  },
 };
 
 export default NextAuth(authOptions);
