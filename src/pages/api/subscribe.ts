a2d05e84b3b13466b1e0396e59312f1472c0571e
Marcos Botene <marcosjbotene@gmail.com>
2023-04-18 22:50:36 -0300

    fix: avoiding user duplication when subscribing to ignews

--- src/pages/api/subscribe.ts
+++ src/pages/api/subscribe.ts
@@ -1,6 +1,16 @@
-import { stripe } from '@/services';
+import { fauna, stripe } from '@/services';
+import { query as q } from 'faunadb';
 import { NextApiRequest, NextApiResponse } from 'next';
 import { getSession } from 'next-auth/react';
+
+interface User {
+  ref: {
+    id: string;
+  };
+  data: {
+    stripe_customer_id: string;
+  };
+}
 
 export default async function subscribe(
   request: NextApiRequest,
@@ -10,12 +20,28 @@ export default async function subscribe(
     const session = await getSession({ req: request });
 
     if (session && session.user?.email) {
-      const stripeCustomer = await stripe.customers.create({
-        email: session.user.email,
-      });
+      const user = await fauna.query<User>(
+        q.Get(q.Match(q.Index('user_by_email'), q.Casefold(session.user.email)))
+      );
+
+      let customerId = user.data.stripe_customer_id;
+
+      if (!customerId) {
+        const stripeCustomer = await stripe.customers.create({
+          email: session.user.email,
+        });
+
+        await fauna.query(
+          q.Update(q.Ref(q.Collection('users'), user.ref.id), {
+            data: { stripe_customer_id: stripeCustomer.id },
+          })
+        );
+
+        customerId = stripeCustomer.id;
+      }
 
       const stripeCheckoutSession = await stripe.checkout.sessions.create({
-        customer: stripeCustomer.id,
+        customer: customerId,
         payment_method_types: ['card'],
         billing_address_collection: 'required',
         line_items: [{ price: 'price_1MpeQ6IxgNCujPqX7M1qTCGf', quantity: 1 }],
