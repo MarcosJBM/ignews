c8b8b7ee3b307544bc274d1e6fa42950a21a5b53
Marcos Botene <marcosjbotene@gmail.com>
2023-09-26 22:08:17 -0300

    refactor: using tailwindcss in sign-in-button component

--- src/components/sign-in-button/index.tsx
+++ src/components/sign-in-button/index.tsx
@@ -2,24 +2,22 @@ import { signIn, signOut, useSession } from 'next-auth/react';
 import { FaGithub } from 'react-icons/fa';
 import { FiX } from 'react-icons/fi';
 
-import styles from './styles.module.scss';
-
 export function SignInButton() {
   const { data: session } = useSession();
 
   return session ? (
     <button
-      className={styles.signInButton}
+      className='flex h-12 items-center justify-center rounded-[3rem] border-0 bg-gray-850 px-6 font-bold text-white transition delay-200 hover:brightness-[0.8]'
       type='button'
       onClick={() => signOut()}
     >
-      <FaGithub color='#04d361' />
+      <FaGithub color='#04d361' className='mr-4 h-5 w-5' />
       {session.user?.name}
-      <FiX className={styles.closeIcon} color='#737380' />
+      <FiX className='ml-4' color='#737380' />
     </button>
   ) : (
     <button
-      className={styles.signInButton}
+      className='flex h-12 items-center justify-center rounded-[3rem] border-0 bg-gray-850 px-6 font-bold text-white transition delay-200 hover:brightness-[0.8]'
       type='button'
       onClick={() => signIn('github')}
     >
