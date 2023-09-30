7937ffe44c8dfbbb61b72858bf01ba4c228ed685
Marcos Botene <marcosjbotene@gmail.com>
2023-09-29 23:23:29 -0300

    refactor: removing unused css in global.css

--- prismicio.ts
+++ prismicio.ts
@@ -3,18 +3,8 @@ import * as prismicNext from '@prismicio/next';
 
 import config from './slicemachine.config.json';
 
-/**
- * The project's Prismic repository name.
- */
 export const repositoryName = config.repositoryName;
 
-/**
- * A list of Route Resolver objects that define how a document's `url` field
- * is resolved.
- *
- * {@link https://prismic.io/docs/route-resolver#route-resolver}
- */
-// TODO: Update the routes array to match your project's route structure.
 const routes: prismic.ClientConfig['routes'] = [
   {
     type: 'post',
@@ -26,12 +16,6 @@ const routes: prismic.ClientConfig['routes'] = [
   },
 ];
 
-/**
- * Creates a Prismic client for the project's repository. The client is used to
- * query content from the Prismic API.
- *
- * @param config - Configuration for the Prismic client.
- */
 export const createClient = (config: prismicNext.CreateClientConfig = {}) => {
   const client = prismic.createClient(repositoryName, {
     routes,
